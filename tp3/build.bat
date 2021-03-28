959f04770fb91437de52228e304d3589f57aeb70
Nhi TRAN <nntran@users.noreply.github.com>
2021-03-28 14:08:03 +0200

    Adaptation script de build pour Windows

--- tp3/build.bat
+++ tp3/build.bat
@@ -6,17 +6,17 @@ SET RESSOURCES_DIR=src\ressources
 
 RMDIR %OUT_CLASSES_DIR% /s /q
 
-# Classes de bases des objets
+REM Classes de bases des objets
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\objet\Point.java
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\objet\Objet.java
 
-# Armes 
+REM Armes 
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\objet\arme\Arme.java
 
-# Avions
+REM Avions
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\objet\avion\Avion.java
 
-# Application
+REM Application
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\Scene.java
 javac -d %OUT_CLASSES_DIR% -cp %OUT_CLASSES_DIR% %SRC_DIR%\fr\ntdt\game\shooter\App.java
 
